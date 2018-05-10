59d1ce4831bf20a1056dfd7b214ea85324c34443
Andrew Schumacher <as228512@ohio.edu>
2018-05-10 10:16:25 -0400

    commiting before heroku

--- frontend/slack_entry.jsx
+++ frontend/slack_entry.jsx
@@ -4,7 +4,7 @@ import configureStore from './store/store';
 import Root from './components/root';
 // import App from './components/app';
 import {createUser, loginUser, logoutUser} from './util/session_api_util';
-import {login} from './actions/session_actions';
+import {login,create, logout} from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -12,6 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.login = login;
+  window.create = create;
+  window.logout = logout;
   // window.loginUser = loginUser;
   // window.logoutUser = logoutUser;
   // window.createUser = createUser;
