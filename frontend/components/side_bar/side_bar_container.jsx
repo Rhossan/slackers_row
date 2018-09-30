8d59518c0e845495393e1b5259e94e628b524c25
Rhossan <rhossan93@gmail.com>
2018-09-30 15:46:34 -0400

    selector for all DMs

--- frontend/components/side_bar/side_bar_container.jsx
+++ frontend/components/side_bar/side_bar_container.jsx
@@ -1,11 +1,14 @@
 import {connect} from 'react-redux';
 import Sidebar from './side_bar';
 import {logout} from '../../actions/session_actions';
-import {selectAllChannels} from '../../reducers/selectors';
+import {selectAllChannels, selectAllDMs} from '../../reducers/selectors';
 import {requestAllChannels, requestChannel, makeChannel} from '../../actions/channel_actions';
 import {openModal, closeModal} from '../../actions/modal_actions';
 
 const mapStateToProps = (state) => {
+  debugger
+  const channels = selectAllChannels(state);
+  const dms = channels.filter(channel => channel.channel_type == 'direct_message');
   return {
     currentUser : state.entities.users[state.session.id],
     channels: selectAllChannels(state)
